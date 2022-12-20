7542cd70704d202e471f426e4cee61402f906948
molode4ik <cs1.6l0vemike@gmail.com>
2022-12-20 09:41:23 +0300

    rework switch cases, add requirements

--- backend/scripts.py
+++ backend/scripts.py
@@ -14,35 +14,26 @@ def cur_data():
 
 
 def get_table_name(user_type: str):
-    match user_type:
-        case 'студент':
-            return 'students'
-        case 'пенсионер':
-            return 'pensioners'
-        case 'пользователь':
-            return 'peoples'
-        case 'школьник':
-            return 'schools'
-        case 'научный работник':
-            return 'scientists'
-        case 'преподаватель':
-            return 'teachers'
+    return {
+        'студент': 'students',
+        'пенсионер': 'pensioners',
+        'пользователь': 'peoples',
+        'школьник': 'schools',
+        'научный работник': 'scientists',
+        'преподаватель': 'teachers'
+    }.get(user_type)
 
 
 def add_user(req_data: dict, table_name: str, connection):
-    match table_name:
-        case 'students':
-            db.add_student(req_data, connection)
-        case 'teachers':
-            db.add_teacher(req_data, connection)
-        case 'peoples':
-            db.add_people(req_data, connection)
-        case 'schools':
-            db.add_school(req_data, connection)
-        case 'scientists':
-            db.add_scientist(req_data, connection)
-        case 'pensioners':
-            db.add_pensioner(req_data, connection)
+    swithcer = {
+        'students': db.add_student(req_data, connection),
+        'teachers': db.add_teacher(req_data, connection),
+        'peoples': db.add_people(req_data, connection),
+        'schools': db.add_school(req_data, connection),
+        'scientists': db.add_scientist(req_data, connection),
+        'pensioners': db.add_pensioner(req_data, connection)
+    }
+    swithcer.get(table_name)
 
 
 def get_not_null_values(data, columns):
